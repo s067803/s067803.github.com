4e4f34568d8dec721871792e2cc61ec83411e271
Naoki Ueda <orange.s067803@gmail.com>
2013-08-17 20:38:58 +0900

    Delete

--- _site/control_git.py
+++ _site/control_git.py
@@ -5,10 +5,8 @@ _add_all = "git add ."
 COMMITMESSAGE = raw_input("commit message: ")
 
 _commit = "git commit -m" + COMMITMESSAGE
-# _add = "git remote add origin https://github.com/s067803/code.git"
 _push = "git push -u origin master"
 
 os.system(_add_all)
 os.system(_commit)
-# os.system(_add)
 os.system(_push)
